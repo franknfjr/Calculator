cf4f23ae01f111ec7eea5f9fc07ce6d369693527
franknfjr <frankdonascimentojr@gmail.com>
2023-02-22 12:47:21 -0300

    basic stacking

--- ContentView.swift
+++ ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let buttons = [
+        ["7", "8", "9", "X"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", "0", ",", "="],
+    ]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack (alignment: .bottom) {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack (spacing: 12) {
+                HStack {
+                    Spacer()
+                    Text("42")
+                        .foregroundColor(.white)
+                        .font(.system(size: 64))
+                }.padding()
+                
+                ForEach(buttons, id: \.self) { row in
+                    HStack (spacing: 12) {
+                        ForEach(row, id: \.self) { button in
+                            Text(button)
+                                .font(.system(size: 32))
+                                .frame(width: self.buttonWidth(), height: self.buttonWidth())
+                                .foregroundColor(.white)
+                                .background(Color.orange)
+                                .cornerRadius(self.buttonWidth())
+                        }
+                    }
+                }
+            }.padding(.bottom)
         }
-        .padding()
+    }
+    
+    func buttonWidth() -> CGFloat {
+        return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
 
